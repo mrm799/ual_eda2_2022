1a901f37b1c93a31002bf0c19e37944db97d9f73
mrm799 <101094455+mrm799@users.noreply.github.com>
2022-04-01 01:18:39 +0200

    Add files via upload

---
+++ practica_1/sources/GenerarJugadoresAleatorios.java
@@ -0,0 +1,28 @@
+package Practica01;
+
+import java.util.ArrayList;
+
+public class GenerarJugadoresAleatorios {
+	
+	public static void main(String[] args) {
+		long inicio;
+		long fin;
+			createNbaRnd(5000000, 15);
+			inicio = System.currentTimeMillis();
+			//SolucionPracticaNba.mejoresJugadores();
+			SolucionPracticaNba.mejoresJugadoresConMejora();
+			fin = System.currentTimeMillis();
+			System.out.println();
+			System.out.println("Tiempo de ejecución:" + (fin - inicio) + " milisegundos");
+	}
+	
+	private static void createNbaRnd(int N, int topN) {
+		SolucionPracticaNba.nbaPlayers = new ArrayList<Player>();
+		for (int i = 0; i < N; i++) {
+			Player p = new Player("Jugador" + i, "", "", (int)(Math.random()*N));
+			SolucionPracticaNba.nbaPlayers.add(p);
+		}
+		SolucionPracticaNba.topN = topN;
+	}
+
+}
